4ced3aae98d9422b98fff76bc1169ea9582c77da
Phoebe Windham <phoebe.frances@outlook.com>
2022-09-26 23:01:07 +0100

    tested route handlers on postman - all working

--- posts/index.js
+++ posts/index.js
@@ -11,8 +11,8 @@ app.get('/posts', (req, res) => {
     res.send(posts)
 });
 
+// used randomBytes to generate a new id to assign to the post that the user is trying to create, we want four bytes of random data so gets a random string
 app.post('/posts', (req, res) => { 
-    // used randomBytes to generate a new id to assign to the post that the user is trying to create, we want four bytes of random data so gets a random string
     const id = randomBytes(4).toString('hex')
     const { title } = req.body; 
 
@@ -26,3 +26,4 @@ app.post('/posts', (req, res) => {
 app.listen(4000, () => { 
     console.log('Listening on 4000')
 })
+
