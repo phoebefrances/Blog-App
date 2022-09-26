3d47a143fb52dccd24a2a15c790f6f12acf8ae13
Phoebe Windham <phoebe.frances@outlook.com>
2022-09-26 22:43:01 +0100

    required title property that the user has sent from the POST request, assigned id and title to our posts object and created a resource to send back the post we just created

--- posts/index.js
+++ posts/index.js
@@ -12,6 +12,13 @@ app.get('/posts', (req, res) => {
 app.post('/posts', (req, res) => { 
     // used randomBytes to generate a new id to assign to the post that the user is trying to create, we want four bytes of random data so gets a random string
     const id = randomBytes(4).toString('hex')
+    const { title } = req.body; 
+
+    posts[id] = {
+        id, title 
+    }
+
+    res.status(201).send(posts[id]);
 });
 
 app.listen(4000, () => { 
