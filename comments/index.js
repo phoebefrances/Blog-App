5cac04345fbd0974c9f02764adc56c5fece81ca3
Phoebe Windham <phoebe.frances@outlook.com>
2022-10-03 12:23:58 +0100

    created ID for each comment using randomBytes and created content object coming from the user request and pulled it out

--- comments/index.js
+++ comments/index.js
@@ -8,6 +8,8 @@ app.use(bodyParser.json());
 const commentsByPostId = {};
 
 app.get('/posts/:id/comments', (req, res) => {
+    const commentId = randomBytes(4).toString('hex');
+    const {content} = req.body; 
 
 }); 
 
