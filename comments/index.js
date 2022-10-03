6795c26071f0b3a3c8c3569034e52b55cfa21b29
Phoebe Windham <phoebe.frances@outlook.com>
2022-10-03 12:20:34 +0100

    created in-memory data structure using an object to store all comments that get created

--- comments/index.js
+++ comments/index.js
@@ -5,6 +5,8 @@ const { randomBytes } = require('crypto');
 const app = express(); 
 app.use(bodyParser.json()); 
 
+const commentsByPostId = {};
+
 app.get('/posts/:id/comments', (req, res) => {
 
 }); 
