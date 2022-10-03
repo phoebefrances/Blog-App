44829ccdd7e4645af5fd0b2ff2cb028ec5d50f8e
Phoebe Windham <phoebe.frances@outlook.com>
2022-10-03 12:40:01 +0100

    added res.status to app.post handler and sent back all comments

--- comments/index.js
+++ comments/index.js
@@ -20,6 +20,8 @@ app.post('/posts/:id/comments', (req, res) => {
     comments.push({id: commentId, content })
 
     commentsByPostId[req.params.id] = comments;
+
+    res.status(201).send(comments); 
 }); 
 
     
