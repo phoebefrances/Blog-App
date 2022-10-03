44bb1559b16c640c7486ee9bcb1cc74e917556ca
Phoebe Windham <phoebe.frances@outlook.com>
2022-10-03 12:29:24 +0100

    created comments to see if we have an array inside of the given post ID provided inside the URL which will either give an array or if undefined give an empty array by default

--- comments/index.js
+++ comments/index.js
@@ -8,13 +8,14 @@ app.use(bodyParser.json());
 const commentsByPostId = {};
 
 app.get('/posts/:id/comments', (req, res) => {
-    const commentId = randomBytes(4).toString('hex');
-    const {content} = req.body; 
-
+    
 }); 
 
 app.post('/posts/:id/comments', (req, res) => {
+    const commentId = randomBytes(4).toString('hex');
+    const {content} = req.body; 
 
+    const comments = commentsByPostId[req.params.id] || [];
 }); 
 
 app.listen(4001, () => {
