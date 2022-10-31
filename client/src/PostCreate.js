221c48199cdcba32a7f1a0533b470abe8569e362
Phoebe Windham <phoebe.frances@outlook.com>
2022-10-31 15:34:56 +0000

    fixed CORS handling request error by npm i cors, and importing it into comments/index.js and posts/index.js and calling cors

--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -1,32 +1,34 @@
-import React, {useState} from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
-export default () => { 
+const PostCreate = () => {
+  const [title, setTitle] = useState("");
 
-    const [title, setTitle] = useState ('');
-    
-    const onSubmit = async (event) => {
-        event.preventDefault();
+  const onSubmit = async (event) => {
+    event.preventDefault();
 
-        await axios.post('https://localhost:4000/posts' , {
-            title 
-        });
-        setTitle('');
-    };
+    await axios.post("http://localhost:4000/posts", {
+      title,
+    });
 
-    return (
+    setTitle("");
+  };
+
+  return (
     <div>
-    <form onSubmit={onSubmit}>
-        <div className='form-group'>
-            <label>Title</label>
-            <input 
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Title</label>
+          <input
             value={title}
-            onChange={e => setTitle(e.target.value)}
-            className='form-control' 
-            />
+            onChange={(e) => setTitle(e.target.value)}
+            className="form-control"
+          />
         </div>
-        <button className='btn btn-primary'>Submit</button>
-    </form>
+        <button className="btn btn-primary">Submit</button>
+      </form>
     </div>
-    );
-}; 
+  );
+};
+
+export default PostCreate;
