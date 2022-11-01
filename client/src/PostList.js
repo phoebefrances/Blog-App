1aa693a1ad341469f6f1f16ce2f2c0aec735c8e8
Phoebe Windham <phoebe.frances@outlook.com>
2022-11-01 16:43:17 +0000

    rendered posts on the page with a function which converts the response object to an array, maps over the array and returns the title of each post, this function is then called as the return for PostList function / component

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,24 +1,38 @@
-import React, {useState, useEffect} from "react";
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-export default () => {
-    const [posts, setPosts] = useState({});
+const PostList = () => {
+  const [posts, setPosts] = useState({});
 
-    const fetchPosts = async () => {
-        const res = await axios.get('http://localhost:4000/posts');
+  const fetchPosts = async () => {
+    const res = await axios.get("http://localhost:4000/posts");
 
-        setPosts(res.data);
-    };
+    setPosts(res.data);
+  };
 
-    useEffect(() => {
-        fetchPosts();
-    }, []); 
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
-    console.log(posts);
-
+  const renderedPosts = Object.values(posts).map((post) => {
     return (
-        <div>
-
+      <div
+        className="card"
+        style={{ width: "30%", marginBottom: "20px" }}
+        key={post.id}
+      >
+        <div className="card-body">
+          <h3>{post.title}</h3>
         </div>
-    )
+      </div>
+    );
+  });
+
+  return (
+    <div className="d-flex flex-row flex-wrap justify-content-between">
+      {renderedPosts}
+    </div>
+  );
 };
+
+export default PostList;
