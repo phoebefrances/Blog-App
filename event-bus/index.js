cfa9cff15bf21a1fac21032ffcc081f5824f706c
Phoebe Windham <phoebe.frances@outlook.com>
2022-11-02 15:11:38 +0000

    when a post is created an event is emitted to the event bus

--- event-bus/index.js
+++ event-bus/index.js
@@ -8,13 +8,20 @@ app.use(bodyparser.json());
 app.post('/events', (req, res) => {
     const event = req.body;
 
-    axios.post('http://localhost:4000/events', event);
-    axios.post('http://localhost:4001/events', event);
-    axios.post('http://localhost:4002/events', event);
-
-    res.send({status: 'ok'})
+    axios.post('http://localhost:4000/events', event).catch((err) => {
+        console.log(err.message);
+      });
+      axios.post('http://localhost:4001/events', event).catch((err) => {
+        console.log(err.message);
+      });
+      axios.post('http://localhost:4002/events', event).catch((err) => {
+        console.log(err.message);
+      });
+      res.send({ status: 'OK' });
 }); 
 
 app.listen(4005, () => {
    console.log('You are listening on port 4005') 
 });
+
+
--- posts/index.js
+++ posts/index.js
@@ -2,6 +2,7 @@ const express = require ('express');
 const bodyParser = require('body-parser')
 const { randomBytes } = require('crypto'); 
 const cors = require('cors'); 
+const axios = require('axios')
 
 const app = express(); 
 app.use(bodyParser.json()); 
@@ -14,7 +15,7 @@ app.get('/posts', (req, res) => {
 });
 
 // used randomBytes to generate a new id to assign to the post that the user is trying to create, we want four bytes of random data so gets a random string
-app.post('/posts', (req, res) => { 
+app.post('/posts', async (req, res) => { 
     const id = randomBytes(4).toString('hex')
     const { title } = req.body; 
 
@@ -22,6 +23,13 @@ app.post('/posts', (req, res) => {
         id, title 
     }
 
+   await axios.post('http://localhost:4005', {
+    type: 'PostCreated',
+    data: {id, title}
+    }).catch((err) => {
+        console.log(err.message);
+    });
+    
     res.status(201).send(posts[id]);
 });
 
